de1181e920eda30669570efc8d85410002ec36e7
Ilikara <3435193369@qq.com>
2025-02-05 21:09:42 +0800

    feat(api): add delFolder api

--- src/components/api/folderApi.ts
+++ src/components/api/folderApi.ts
@@ -2,7 +2,7 @@
  * @Author: Ilikara 3435193369@qq.com
  * @Date: 2025-02-03 13:00:47
  * @LastEditors: Ilikara 3435193369@qq.com
- * @LastEditTime: 2025-02-04 18:35:42
+ * @LastEditTime: 2025-02-05 20:15:40
  * @FilePath: /SynapForest/src/components/api/folderApi.ts
  * @Description: 
  * 
@@ -195,5 +195,57 @@ export const fetchFolders = async ({
     } catch (error) {
         console.error('Error fetching folders:', error);
         return [];
+    }
+};
+
+/**
+ * 将指定项目永久删除
+ * @param folderId 需要删除的项目ID
+ * @param deleteItems 是否删除关联的文件
+ * @throws 如果请求失败或服务器返回错误，抛出异常
+ */
+export const delFolder = async ({
+    folderId,
+    deleteItems
+}: {
+    folderId: string;
+    deleteItems: boolean;
+}) => {
+    try {
+        const address = get(serverAddress);
+        const authToken = get(token);
+
+        if (!address || !authToken) {
+            throw new Error('Server address or token is missing');
+        }
+
+        const body = JSON.stringify({
+            folderId,
+            deleteItems,
+        });
+
+        const response = await fetch(`${address}/api/folder/delete`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `${authToken}`,
+            },
+            body: body,
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to delete folders');
+        }
+
+        const result = await response.json();
+
+        if (result.status !== 'success') {
+            throw new Error(result.message || 'Unknown error occurred');
+        }
+
+        return;
+    } catch (error) {
+        console.error('Error moving folders to trash:', error);
+        throw error;
     }
 };
